18aed7add5b382f917355c244dbcb0ea8e30e6ca
Dan <zexion313@gmail.com>
2023-11-16 00:00:17 +0800

    Updated Form 1 and Form 3

--- BHAS V.2.0/BHAS V.2.0/Form1.Designer.vb
+++ BHAS V.2.0/BHAS V.2.0/Form1.Designer.vb
@@ -24,8 +24,11 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnadmin = New System.Windows.Forms.Button()
+        Me.btnguest = New System.Windows.Forms.Button()
+        Me.btnexit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -33,55 +36,101 @@ Partial Class Form1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(-2, -1)
+        Me.PictureBox1.Location = New System.Drawing.Point(-3, -1)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(421, 333)
+        Me.PictureBox1.Size = New System.Drawing.Size(561, 410)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
-        'Button1
+        'btnadmin
         '
-        Me.Button1.BackColor = System.Drawing.SystemColors.HotTrack
-        Me.Button1.Font = New System.Drawing.Font("Elephant", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.ForeColor = System.Drawing.Color.White
-        Me.Button1.Location = New System.Drawing.Point(146, 179)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(119, 38)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Administrator"
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.btnadmin.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.btnadmin.Font = New System.Drawing.Font("Elephant", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnadmin.ForeColor = System.Drawing.Color.White
+        Me.btnadmin.Location = New System.Drawing.Point(28, 297)
+        Me.btnadmin.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnadmin.Name = "btnadmin"
+        Me.btnadmin.Size = New System.Drawing.Size(159, 47)
+        Me.btnadmin.TabIndex = 1
+        Me.btnadmin.Text = "Administrator"
+        Me.btnadmin.UseVisualStyleBackColor = False
         '
-        'Button2
+        'btnguest
         '
-        Me.Button2.BackColor = System.Drawing.Color.Firebrick
-        Me.Button2.Font = New System.Drawing.Font("Elephant", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.ForeColor = System.Drawing.Color.White
-        Me.Button2.Location = New System.Drawing.Point(146, 223)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(119, 38)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Guest"
-        Me.Button2.UseVisualStyleBackColor = False
+        Me.btnguest.BackColor = System.Drawing.Color.SeaGreen
+        Me.btnguest.Font = New System.Drawing.Font("Elephant", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnguest.ForeColor = System.Drawing.Color.White
+        Me.btnguest.Location = New System.Drawing.Point(195, 297)
+        Me.btnguest.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnguest.Name = "btnguest"
+        Me.btnguest.Size = New System.Drawing.Size(159, 47)
+        Me.btnguest.TabIndex = 2
+        Me.btnguest.Text = "Guest"
+        Me.btnguest.UseVisualStyleBackColor = False
+        '
+        'btnexit
+        '
+        Me.btnexit.BackColor = System.Drawing.Color.Firebrick
+        Me.btnexit.Font = New System.Drawing.Font("Elephant", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnexit.ForeColor = System.Drawing.Color.White
+        Me.btnexit.Location = New System.Drawing.Point(362, 297)
+        Me.btnexit.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(159, 47)
+        Me.btnexit.TabIndex = 3
+        Me.btnexit.Text = "Exit"
+        Me.btnexit.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei UI", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(509, 381)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 19)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "2023"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei UI", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(4, 383)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(197, 19)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Blue House Apartment System"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(416, 328)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(555, 404)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnexit)
+        Me.Controls.Add(Me.btnguest)
+        Me.Controls.Add(Me.btnadmin)
         Me.Controls.Add(Me.PictureBox1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Blue House Apartment System"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents btnadmin As Button
+    Friend WithEvents btnguest As Button
+    Friend WithEvents btnexit As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
--- BHAS V.2.0/BHAS V.2.0/Form3.Designer.vb
+++ BHAS V.2.0/BHAS V.2.0/Form3.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form3
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,41 +20,77 @@ Partial Class Form3
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form3))
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnexit = New System.Windows.Forms.Button()
+        Me.btnguest = New System.Windows.Forms.Button()
+        Me.btnadmin = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(-4, -2)
+        Me.PictureBox1.Location = New System.Drawing.Point(279, 25)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(653, 389)
+        Me.PictureBox1.Size = New System.Drawing.Size(272, 200)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
-        'Button1
+        'btnexit
         '
-        Me.Button1.Location = New System.Drawing.Point(459, 328)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(135, 42)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnexit.BackColor = System.Drawing.Color.Firebrick
+        Me.btnexit.Font = New System.Drawing.Font("Elephant", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnexit.ForeColor = System.Drawing.Color.White
+        Me.btnexit.Location = New System.Drawing.Point(553, 276)
+        Me.btnexit.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(159, 47)
+        Me.btnexit.TabIndex = 6
+        Me.btnexit.Text = "Exit"
+        Me.btnexit.UseVisualStyleBackColor = False
+        '
+        'btnguest
+        '
+        Me.btnguest.BackColor = System.Drawing.Color.SeaGreen
+        Me.btnguest.Font = New System.Drawing.Font("Elephant", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnguest.ForeColor = System.Drawing.Color.White
+        Me.btnguest.Location = New System.Drawing.Point(328, 276)
+        Me.btnguest.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnguest.Name = "btnguest"
+        Me.btnguest.Size = New System.Drawing.Size(159, 47)
+        Me.btnguest.TabIndex = 5
+        Me.btnguest.Text = "Guest"
+        Me.btnguest.UseVisualStyleBackColor = False
+        '
+        'btnadmin
+        '
+        Me.btnadmin.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.btnadmin.Font = New System.Drawing.Font("Elephant", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnadmin.ForeColor = System.Drawing.Color.White
+        Me.btnadmin.Location = New System.Drawing.Point(113, 276)
+        Me.btnadmin.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnadmin.Name = "btnadmin"
+        Me.btnadmin.Size = New System.Drawing.Size(159, 47)
+        Me.btnadmin.TabIndex = 4
+        Me.btnadmin.Text = "Administrator"
+        Me.btnadmin.UseVisualStyleBackColor = False
         '
         'Form3
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(643, 383)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(857, 471)
+        Me.Controls.Add(Me.btnexit)
+        Me.Controls.Add(Me.btnguest)
+        Me.Controls.Add(Me.btnadmin)
         Me.Controls.Add(Me.PictureBox1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Form3"
         Me.Text = "Main Interface"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -63,5 +99,7 @@ Partial Class Form3
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnexit As Button
+    Friend WithEvents btnguest As Button
+    Friend WithEvents btnadmin As Button
 End Class
